cf1a18447f3e2a6d122eff3006de82d78e9e6fd1
Jip Moors <jip@yoast.com>
2016-03-02 16:35:21 +0100

    comments

--- tests/notifications/test-class-yoast-notification-center.php
+++ tests/notifications/test-class-yoast-notification-center.php
@@ -10,21 +10,21 @@ class Test_Yoast_Notification_Center extends WPSEO_UnitTestCase {
 	/**
 	 * Tests:
 	 *  + Set notifications to storage
-	 *  Get notifications from storage
+	 *  - Get notifications from storage
 	 *  + Clear stored notificatoins
 	 *  + register_notifications
 	 *  ajax_dismiss_notification
 	 *  + is_notification_dismissed
-	 *  maybe_dismiss_notification
+	 *  + maybe_dismiss_notification
 	 *      -- extensive arguments
-	 *  clear_dismissal
+	 *  + clear_dismissal
 	 *  + add_notification
 	 *  + get_notification_by_id
-	 *  display_notifications
+	 *  + display_notifications
 	 *
-	 * + Notification display for user is called
-	 * Notifier resolve is called
-	 * Notifier notification is added
+	 *  + Notification display for user is called
+	 *  + Notifier resolve is called
+	 *  + Notifier notification is added
 	 */
 
 	/**
